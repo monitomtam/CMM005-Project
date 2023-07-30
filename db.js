61b599d9c80ccdf3ba39831fb5261d6a135a1f0f
Monisola <monituyi@yahoo.com>
2023-07-30 23:19:33 +0100

    Third Commit

---
+++ db.js
@@ -0,0 +1,25 @@
+var data= [
+    {
+    "qs": "Q1. what is 2+2?",
+    "options": [
+        "5",
+        "4",
+        "9",
+        "1"
+    ],
+        "answerId": 1    
+    },
+
+    {
+        "qs": "Q2. What is 5+5",
+        "options": [      
+            "5",
+            "4",
+            "9",
+            "10"
+        ],
+        "answerId": 3 
+    }
+]
+
+
